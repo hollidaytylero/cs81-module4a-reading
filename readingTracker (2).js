ee51e0a2e594db20b8cba54ed6b8e1cfb5fc22f7
hollidaytylero <holliday_tyler_oli01@student.smc.edu>
2026-07-06 21:08:31 -0700

    Added in 10 comments into code

--- readingTracker (2).js
+++ readingTracker (2).js
@@ -2,6 +2,7 @@
 Purpose: To show a weekly reading log
 Inputs: day, title, minutes
 Results: day of the week, book title, minutes reading
+//Weekly reading log with 3 different objects each day
 const readingLog = [
   { day: "Monday", book: "Dune", minutes: 30 },
   { day: "Tuesday", book: "1984", minutes: 20 },
@@ -12,7 +13,7 @@ const readingLog = [
 Purpose: Adds a new reading entry into the log
 Inputs: day, title, minutes
 Results: day of the week, book title, minutes reading
-
+//Creates a new object using the function parameters of day, book, minutes
 // Adds a new reading entry to the log
 function addReadBook(day, book, minutes) {
   const newEntry = { day, book, minutes };
@@ -21,7 +22,7 @@ function addReadBook(day, book, minutes) {
 Purpose: Calculates the total number of minutes spent on reading.
 Inputs: entries in minutes
 Results: total time reading in a final numerical value
-
+// Goes through and adds in all of the reading entries from the inputs
 // Returns total minutes spent reading all week
 function totalReadingMinutes(log) {
   let total = 0;
@@ -37,6 +38,7 @@ Outputs: Title of the book read most frequently
 // Returns the book read most frequently
 function mostReadBook(log) {
   const bookCounts = {};
+  //If book has not been counted yet, start at 1.
   for (let entry of log) {
     if (!bookCounts[entry.book]) {
       bookCounts[entry.book] = 1;
@@ -44,12 +46,13 @@ function mostReadBook(log) {
       bookCounts[entry.book]++;
     }
   }
-
+//Stores and displays the book with highest count
   let maxBook = null;
   let maxCount = 0;
   for (let book in bookCounts) {
     if (bookCounts[book] > maxCount) {
       maxBook = book;
+      //Updates the most-read book
       maxCount = bookCounts[book];
     }
   }
@@ -59,6 +62,7 @@ function mostReadBook(log) {
 // Prints a summary of minutes read per day
 function printDailySummary(log) {
   for (let entry of log) {
+    //Readable dashboard summary message
     console.log(`${entry.day}: ${entry.minutes} mins reading "${entry.book}"`);
   }
 }
