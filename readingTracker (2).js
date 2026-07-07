50b493559126237b62e000984c0345603b3f3019
hollidaytylero <holliday_tyler_oli01@student.smc.edu>
2026-07-06 20:52:00 -0700

    Purpose summaries for each different function

--- readingTracker (2).js
+++ readingTracker (2).js
@@ -1,4 +1,7 @@
 // Weekly reading log
+Purpose: To show a weekly reading log
+Inputs: day, title, minutes
+Results: day of the week, book title, minutes reading
 const readingLog = [
   { day: "Monday", book: "Dune", minutes: 30 },
   { day: "Tuesday", book: "1984", minutes: 20 },
@@ -6,12 +9,18 @@ const readingLog = [
   { day: "Thursday", book: "The Hobbit", minutes: 40 },
   { day: "Friday", book: "1984", minutes: 15 }
 ];
+Purpose: Adds a new reading entry into the log
+Inputs: day, title, minutes
+Results: day of the week, book title, minutes reading
 
 // Adds a new reading entry to the log
 function addReadBook(day, book, minutes) {
   const newEntry = { day, book, minutes };
   readingLog.push(newEntry);
 }
+Purpose: Calculates the total number of minutes spent on reading.
+Inputs: entries in minutes
+Results: total time reading in a final numerical value
 
 // Returns total minutes spent reading all week
 function totalReadingMinutes(log) {
@@ -21,6 +30,9 @@ function totalReadingMinutes(log) {
   }
   return total;
 }
+Purpose: Determines which book is read the most frequently according to the log.
+Inputs: Different reading entries of time
+Outputs: Title of the book read most frequently
 
 // Returns the book read most frequently
 function mostReadBook(log) {
@@ -50,9 +62,15 @@ function printDailySummary(log) {
     console.log(`${entry.day}: ${entry.minutes} mins reading "${entry.book}"`);
   }
 }
+Purpose: Gives a daily dashboard or summary of reading.
+Input: A variety of entries based on what has been read.
+Output: Diary of reading information/summary of each day.
 
 // Example usage
 addReadBook("Saturday", "Dune", 50);
 printDailySummary(readingLog);
 console.log("Total minutes read:", totalReadingMinutes(readingLog));
 console.log("Most read book:", mostReadBook(readingLog));
+
+Suggestion for Improvement: 
+Test Case: 
